2292e523fa6d0a805a8eafd3eb06a71d1cffbd5d
Lino Moretto <lino.moretto@gmail.com>
2010-08-31 12:42:34 +0200

    Minor paranoic changes

--- lib/generators/acts_as_esagon_generator.rb
+++ lib/generators/acts_as_esagon_generator.rb
@@ -22,15 +22,15 @@ class ActsAsEsagonGenerator < Rails::Generators::Base
   
   def list_of_models(type = nil, force = false)
     all_models = Dir.glob( File.join( Rails.root, 'app', 'models', '*.rb') ).map { |path| path[/.+\/(.+).rb/,1] }
-    all_models = all_models.select { |m| m.classify == name } if !name.blank?
+    all_models = all_models.select { |m| m.classify == name } unless name.blank?
     ar_models = all_models.select { |m| m.classify.constantize < ActiveRecord::Base }
     ar_models = ar_models.select { |model| model.classify.constantize.methods.select { |m| m == :has_esagon_bindings? }.length > 0 }
-    ar_models = ar_models.select { |model| model.classify.constantize.has_esagon_bindings?(type) } if !type.nil?
-    ar_models = ar_models.select{ |model| !model.classify.constantize.columns_hash.has_key?("#{model}_LM") } if options.migration? && !force
+    ar_models = ar_models.select { |model| model.classify.constantize.has_esagon_bindings?(type) } unless type.nil?
+    ar_models = ar_models.select { |model| !model.classify.constantize.columns_hash.has_key?("#{model}_LM") } if options.migration? && !force
     ar_models
   end
   
   def migration_name
-    'AddEsagonBindingsTo' + list_of_models.map { |m| m.classify }.join('And')
+    "AddEsagonBindingsTo#{list_of_models.map { |m| m.classify }.join('And')}"
   end
 end
