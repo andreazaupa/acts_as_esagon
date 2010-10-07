665e7a4a5ebe65fe4baa90a38bdd9cac9887a0cf
Lino Moretto <lino.moretto@gmail.com>
2010-10-07 13:04:54 +0200

    Bug fix for singular entity names handling

--- lib/generators/acts_as_esagon_generator.rb
+++ lib/generators/acts_as_esagon_generator.rb
@@ -13,7 +13,7 @@ class ActsAsEsagonGenerator < Rails::Generators::Base
       if list_of_models.size > 0
         template "migration.rb", "db/migrate/#{Time.now.strftime('%Y%m%d%H%M%S')}_#{migration_name.underscore}.rb"
       else
-        puts "No need to migrate: #{list_of_models(nil, true).map { |m| m.classify }.join(',')}"
+        puts "No need to migrate: #{list_of_models(nil, true).map { |m| m.camelize }.join(',')}"
       end
     end
   end
@@ -22,15 +22,15 @@ class ActsAsEsagonGenerator < Rails::Generators::Base
   
   def list_of_models(type = nil, force = false)
     all_models = Dir.glob( File.join( Rails.root, 'app', 'models', '*.rb') ).map { |path| path[/.+\/(.+).rb/,1] }
-    all_models = all_models.select { |m| m.classify == name } unless name.blank?
-    ar_models = all_models.select { |m| m.classify.constantize < ActiveRecord::Base }
-    ar_models = ar_models.select { |model| model.classify.constantize.methods.select { |m| m == :has_esagon_bindings? }.length > 0 }
-    ar_models = ar_models.select { |model| model.classify.constantize.has_esagon_bindings?(type) } unless type.nil?
-    ar_models = ar_models.select { |model| !model.classify.constantize.columns_hash.has_key?("#{model.classify.constantize.esagon_binding_properties[:name]}_LM") } if options.migration? && !force
+    all_models = all_models.select { |m| m.camelize == name } unless name.blank?
+    ar_models = all_models.select { |m| m.camelize.constantize < ActiveRecord::Base }
+    ar_models = ar_models.select { |model| model.camelize.constantize.methods.select { |m| m == :has_esagon_bindings? }.length > 0 }
+    ar_models = ar_models.select { |model| model.camelize.constantize.has_esagon_bindings?(type) } unless type.nil?
+    ar_models = ar_models.select { |model| !model.camelize.constantize.columns_hash.has_key?("#{model.camelize.constantize.esagon_binding_properties[:name]}_LM") } if options.migration? && !force
     ar_models
   end
   
   def migration_name
-    "AddEsagonBindingsTo#{list_of_models.map { |m| m.classify }.join('And')}".slice(0, 127)
+    "AddEsagonBindingsTo#{list_of_models.map { |m| m.camelize }.join('And')}".slice(0, 127)
   end
 end
--- lib/generators/templates/migration.rb
+++ lib/generators/templates/migration.rb
@@ -1,13 +1,13 @@
 class <%= migration_name %> < ActiveRecord::Migration
   
 <% list_of_models.each do |model| -%>
-  class <%= model.classify %> < ActiveRecord::Base
+  class <%= model.camelize %> < ActiveRecord::Base
   end
 <% end -%>
 
   def self.up
 <% list_of_models.each do |model| -%>
-<% klass = model.classify.constantize -%>
+<% klass = model.camelize.constantize -%>
     say_with_time 'Adding Esagon fields to <%= klass %>...' do
       change_table(:<%= klass.table_name %>) do |t|
          t.datetime :<%= klass.esagon_binding_properties[:name] %>_LM
@@ -20,8 +20,8 @@ class <%= migration_name %> < ActiveRecord::Migration
   
   def self.down
 <% list_of_models.each do |model| -%>
-<% klass = model.classify.constantize -%>
-    say_with_time 'Removing Esagon fields from <%= model.classify %>...' do
+<% klass = model.camelize.constantize -%>
+    say_with_time 'Removing Esagon fields from <%= model.camelize %>...' do
 <% ['LM', 'OW', 'OL'].each do |e| -%>
       remove_column :<%= klass.table_name %>, :<%= "#{klass.esagon_binding_properties[:name]}_#{e}" %>
 <% end -%>
